653a39f0ef5e4f24032234b804aa3b75ca3ebcb6
M.R.C.D. Abeyrathna <110781133+ChathuAbeyrathna@users.noreply.github.com>
2024-01-23 17:42:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import {Navbar} from './Component/Navbar/Navbar';
+import {Search} from './Component/Search/Search';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import {Home} from './Pages/Home';
 import {Resources} from './Pages/Resources/Resources';
+import {Sensors} from './Pages/Resources/Sensors';
 import {Projects} from './Pages/Projects';
 import {Blogs} from './Pages/Blogs';
 import {Shopping} from './Pages/Shopping';
@@ -11,15 +13,18 @@ import {Forum} from './Pages/Forum';
 import {AboutUs} from './Pages/AboutUs';
 import {LogIn} from './Pages/LogIn';
 import {SignUp} from './Pages/SignUp';
+import {Footer} from './Component/Footer/Footer';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Navbar/>
+      <Search/>
       <Routes>
           <Route path='/home'element={<Home/>}/>
           <Route path='/resources'element={<Resources/>}/>
+            <Route path='/sensors' element={<Sensors/>}/>
           <Route path='/projects'element={<Projects/>}/>
           <Route path='/blogs'element={<Blogs/>}/>
           <Route path='/shopping'element={<Shopping/>}/>
@@ -28,6 +33,7 @@ function App() {
           <Route path='/signup'element={<SignUp/>}/>
           <Route path='/login'element={<LogIn/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
