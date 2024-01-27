9222f89a876002c7ee0094edcf88a35f4682eac4
SewminiAmanda <110555098+SewminiAmanda@users.noreply.github.com>
2024-01-28 00:19:44 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -24,4 +24,24 @@ const UserSchema = new mongoose.Schema({
     },
     })
 
+    
+    UserSchema.statics.findOrCreate = async function (profile, cb) {
+        try {
+          const user = await this.findOne({ googleId: profile.id });
+          if (user) {
+            return cb(null, user);
+          } else {
+            const newUser = new this({
+              googleId: profile.id,
+              // Add any other relevant fields from the Google profile
+            });
+            const savedUser = await newUser.save();
+            return cb(null, savedUser);
+          }
+        } catch (err) {
+          return cb(err, null);
+        }
+      };
+
+
 module.exports=mongoose.model('User',UserSchema)
